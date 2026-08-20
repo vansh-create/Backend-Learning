b6b1bf5578b2d32bc0b00bd25a74de532e088683
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-20 23:03:47 +0530

    Applied Get and Delete Request

--- CRUD Using Backend/src/app.js
+++ CRUD Using Backend/src/app.js
@@ -7,8 +7,29 @@ let notes = []
 app.post("/", (req, res)=>{
     notes.push(req.body)
 
-    res.status(201)
+    res.status(201).json({
+        "message":"note created successfully"
+    })
     console.log(notes)
+})
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        "message":"note send successfully",
+        "notes":notes
+    })
+})
+
+app.get("/:index", (req, res)=>{
+
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(200).json({
+        "message":"note deleted successfully",
+        "notes":notes
+    })
 })
 
 
