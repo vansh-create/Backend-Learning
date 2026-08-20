e25be73c4454ccc1fdd092f55e00461ff815b962
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-20 20:59:53 +0530

    Request received from postman

--- CRUD Using Backend/server.js
+++ CRUD Using Backend/server.js
@@ -1,9 +1,5 @@
 const app = require("./src/app")
 
-app.get("/", (req, res)=>{
-    res.send("Hello Vansh");
-})
-
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 })
--- CRUD Using Backend/src/app.js
+++ CRUD Using Backend/src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
+app.post("/", (req, res)=>{
+    console.log(req.body)
+})
+
 module.exports = app
