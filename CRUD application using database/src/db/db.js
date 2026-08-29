a6b4b6bdae328644509165c64685ad06a58965e2
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-30 01:05:33 +0530

    database connected succesfully

--- CRUD application using database/src/db/db.js
+++ CRUD application using database/src/db/db.js
@@ -1,7 +1,10 @@
+const dns = require("dns");
 const mongoose = require("mongoose")
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://gameonews8_db_user:VVmaUlsOlV7Vxriq@backend-first-project.4sf6awt.mongodb.net/notesDB")
+    await mongoose.connect("mongodb+srv://gameonews8_db_user:VVmaUlsOlV7Vxriq@backend-first-project.4sf6awt.mongodb.net/notesdb")
 
     console.log("Database Connected")
 }
