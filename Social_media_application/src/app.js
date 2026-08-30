d72219a27c66eecb0163b14e770518a7d472f7c2
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-30 18:27:19 +0530

    api created for post creating and post fetching

--- Social_media_application/src/app.js
+++ Social_media_application/src/app.js
@@ -4,5 +4,29 @@ const app = express();
 
 app.use(express.json())
 
+app.post("/create-post", async (req, res) => {
+    const img = req.body.img
+    const caption = req.body.caption
+
+    await postModel.create({
+        img: img,
+        caption: caption
+    })
+
+    res.status(201).json({
+        "message": "post created successfully"
+    })
+})
+
+app.get("/feed", async (req, res) => {
+
+    const data = await postModel.find()
+
+    res.status(200).json({
+        "message": "post fetched successfully",
+        "data": data
+    })
+})
+
 
 module.exports = app
