df580a48bf784d6794c227451e10bed0dd9a4d58
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-31 16:37:00 +0530

    folder wise routes and controller setup

--- Authentication_system/src/app.js
+++ Authentication_system/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const userModel = require("./models/user.model")
+const authRoutes = require("./routes/auth.routes")
 
 const app = express()
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
 
 module.exports = app
---
+++ Authentication_system/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+const userModel = require("../models/user.model")
+
+async function registerUser(req, res) {
+   const {username, email, password}  = req.body
+}
+
+module.exports = {registerUser}
