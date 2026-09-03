a9550a436f1d30b8db63cea0fcd163597bbe6e53
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-09-03 23:21:52 +0530

    simple register api created

--- music_project/src/controllers/auth.controller.js
+++ music_project/src/controllers/auth.controller.js
@@ -1,27 +1,35 @@
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
 
 async function registerUser(req, res) {
-    let user;
-
     const { username, email, password, role } = req.body
 
-    try {
-        user = await userModel.create({
-            username: username,
-            email: email,
-            password: password,
-            role: role
-        })
-    } catch (error) {
-        res.status(404).json({
-            "message": "User already registerd."
+    const isUserExist = await userModel.findOne({
+        $or: [
+            { username }, { email }
+        ]
+    })
+
+    if (isUserExist) {
+        return res.status(201).json({
+            "message": "User already exist.",
         })
     }
 
+    const user = await userModel.create({
+        username: username,
+        email: email,
+        password: password,
+        role: role
+    })
+
+    const token = jwt.sign({id: user._id, role: user.role }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
 
     res.status(201).json({
         "message": "User registerd successfully",
-        "user": user
+        "user":user
     })
 }
 
