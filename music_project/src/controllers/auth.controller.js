7cbfda11caac1cebaddfcb1bb389baea223f6bca
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-09-04 00:15:06 +0530

    login api created

--- music_project/src/controllers/auth.controller.js
+++ music_project/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs")
 
 
 async function registerUser(req, res) {
-    const { username, email, password, role ="user"} = req.body
+    const { username, email, password, role = "user" } = req.body
 
     const isUserExist = await userModel.findOne({
         $or: [
@@ -18,24 +18,64 @@ async function registerUser(req, res) {
             "message": "User already exist.",
         })
     }
-
+    const hash = await bcrypt.hash(password, 10)
     const user = await userModel.create({
         username: username,
         email: email,
-        password: password,
+        password: hash,
         role: role
     })
 
-    const hash = await bcrypt.hash(password, 10)
 
-    const token = jwt.sign({id: user._id, role: user.role }, process.env.JWT_SECRET)
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET)
 
     res.cookie("token", token)
 
     res.status(201).json({
         "message": "User registerd successfully",
-        "user":user
+        "user": user
     })
 }
 
-module.exports = { registerUser }
+
+async function loginUser(req, res) {
+    const { username, email, password } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            { username }, { email }
+        ]
+    })
+
+    if (!user) {
+         return res.status(201).json({
+            "message": "Invalid Credientials.",
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(201).json({
+            "message": "Invalid Credientials.",
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+
+
+    res.status(200).json({
+        "message": "Login succefully",
+        "user": user
+    })
+}
+
+module.exports = { registerUser, loginUser }
+
--- music_project/src/routes/auth.routes.js
+++ music_project/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller")
 const router = express.Router()
 
 router.post("/register", authController.registerUser)
+router.post("/login", authController.loginUser)
 
 module.exports = router
